3de2bbb4e45daa1f584eaa7590b6a976192b444a
poulet_a <poulet_a@epitech.eu>
2014-08-22 11:44:47 +0200

    All StringHelper tested

--- test/test_stringhelper.rb
+++ test/test_stringhelper.rb
@@ -39,14 +39,35 @@ class StringHelperTest < Minitest::Test
     assert_equal(",1,1".to_f, 0.1)
   end
 
+  def test_to_i_wd
+    assert_equal("1 1".to_i_wd(), 11)
+    assert_equal("06.08.68".to_i_wd("."), 60868)
+    assert_equal("06.08.68".to_i_wd("\. \t\-"), 60868)
+  end
+
+  def test_to_t
+    assert_equal("true".to_t, true)
+    assert_equal("false".to_t, false)
+    assert_equal("truex".to_t, nil)
+    assert_equal("xfalsex".to_t, nil)
+    assert_equal("".to_t, nil)
+  end
+
   def test_static
     assert_equal("bonjour".static(1, " "), "b")
     assert_equal("bonjour".static(10, " "), "bonjour   ")
   end
 
   def test_get_int
-    assert_equal("ea ze 13e12 à nnazdaz d".get_int(), "1312")
-    assert_equal("ea\n ze 13\n12\n à nnazdaz\n\t\n\r d".get_int(), "1312")
+    assert_equal("1312".get_int(), "1312")
+    assert_equal("ea -ze 13e12 à nnazdaz d".get_int(), "-1312")
+    assert_equal("ea\n ze +13\n12\n à nnazdaz\n\t\n\r d".get_int(), "+1312")
+  end
+
+  def test_get_int
+    assert_equal("13,12".get_float(), "13,12")
+    assert_equal("ea -ze 13e.12 à nnazdaz d".get_float(), "-13.12")
+    assert_equal("ea\n ze +13\n.12\n à nnazdaz\n\t\n\r d".get_float(), "+13.12")
   end
 
 end
