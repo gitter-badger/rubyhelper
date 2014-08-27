09cc4d60b1c608461a7d859a8d020bdda98efaea
poulet_a <poulet_a@epitech.eu>
2014-08-27 10:22:55 +0200

    add hashhelper

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ rubyhelper
 ======
 
 [![Gem Version](https://badge.fury.io/rb/rubyhelper.svg)](http://badge.fury.io/rb/rubyhelper)
+
+The following sources code is not only my stuff, but also an implementation of idea found on stackoverflow, ...
+
---
+++ lib/rubyhelper/hashhelper.rb
@@ -0,0 +1,18 @@
+module HashHelper
+  # Return a hash that includes everything but the given keys.
+  def except(*keys)
+    return self.dup.except!(*keys)
+  end
+
+  # Replaces the hash without the given keys.
+  def except!(*keys)
+    keys.each { |key| self.delete(key) }
+    return self
+  end
+
+end
+
+class Hash
+  prepend HashHelper
+end
+
