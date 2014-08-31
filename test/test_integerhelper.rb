72376b4b9ea2b3e3535c697f2e0260b46fb860b6
poulet_a <poulet_a@epitech.eu>
2014-08-31 16:58:23 +0200

    Add min and max to Integer - add function integer.min n and integer.max n returns - add test and documentation to integer.min and max

---
+++ test/test_integerhelper.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'rubyhelper'
+
+class IntegerHelperTest < Minitest::Test
+  
+  def test_min
+    assert_equal(2, 1.min(2))
+    assert_equal(1, 1.min(1))
+    assert_equal(1, 1.min(0))
+    assert_equal(0, (-1).min(0))
+    assert_equal(1, 0.min(1))
+  end
+
+  def test_max
+    assert_equal(1, 1.max(2))
+    assert_equal(1, 1.max(1))
+    assert_equal(0, 1.max(0))
+    assert_equal(-1, (-1).max(0))
+    assert_equal(0, 0.max(1))
+  end
+
+end
