c5e1c729ca70ae82dde55a0c252421ee678ad87d
poulet_a <poulet_a@epitech.eu>
2014-09-01 20:17:35 +0200

    add versionhelper

--- lib/rubyhelper/versionhelper.rb
+++ lib/rubyhelper/versionhelper.rb
@@ -1,10 +1,33 @@
 #encoding: utf-8
 
+require 'pry'
+
 module VersionHelper
 
   class Version
+
+    attr_accessor :v
+
     def initialize(*arg)
-      @v = Array.new
+      @v = []
+      if arg.size == 1
+        v = arg.first
+        case v.class.to_s
+        when 'String'
+          v.gsub!(/\D/, '.')
+          v.gsub!(/\A\D/, '')
+          @v = v.split('.').map{|e| e.to_i}
+        when 'Array'
+          @v = v.map{|e| e.to_i}
+        when 'Fixnum'
+          @v << v.to_i
+        else
+          raise ArgumentError, v.class.to_s
+        end
+      end
+      if arg.size > 1
+        @v = arg.map{|e| e.to_i}
+      end
     end
 
     def to_a
@@ -16,27 +39,34 @@ module VersionHelper
     end
 
     def to_h
-      return Version.to_s(self)
+      return Version.to_h(self)
     end
 
     def to_i
-      return Version.to_s(self)
+      return Version.to_i(self)
     end
 
     def self.to_a(version)
       raise ArgumentError unless version.is_a? Version
+      return version.v.dup
     end
 
     def self.to_s(version)
       raise ArgumentError unless version.is_a? Version
+      return version.v.join('.')
     end
 
+    #Not work
     def self.to_h(version)
       raise ArgumentError unless version.is_a? Version
+      return nil
     end
 
     def self.to_i(version)
       raise ArgumentError unless version.is_a? Version
+      i = 0
+      version.v.each{|e| i = i * 10 + e}
+      return i
     end
   end
 
