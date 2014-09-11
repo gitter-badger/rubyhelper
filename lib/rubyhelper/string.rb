cbb41357185703f952f951eb485287c8f6a192c5
Arthur Poulet <arthur.poulet@cryptolab.net>
2014-09-11 19:46:55 +0200

    Remove empty file, clear string.rb

--- lib/rubyhelper/string.rb
+++ lib/rubyhelper/string.rb
@@ -110,24 +110,6 @@ module StringHelper
     return s.to_case(case_mod)
   end
 
-  # improvement of to_f to count "," caracter as "."
-  #
-  # @return [Float] like {Integer#to_f}
-  def to_fi
-    return self.gsub(',', '.').to_f
-  end
-
-  # to_i with delimiter to remove
-  # Example : "12.000.000".to_ii => 12000000
-  #
-  # @param char [String] char to delete (default : ' ')
-  # @raise [ArgumentError] If (param char) is not a String
-  # @return [Integer] like {Integer#to_i]
-  def to_ii(char=' ')
-    raise ArgumentError, "Argument is not a String" unless char.is_a? String
-    self.delete(char).to_i
-  end
-
   # CRYXOR (one time pad dirt application)
   #
   # @raise [ArgumentError] if key is not a valid String
@@ -226,40 +208,6 @@ module StringHelper
     return (self == "false")
   end
 
-  # get only the digits and + - symbols in the string
-  # you should see also {#get_float}
-  #
-  # @param sign (true/false) if true, keep the - and + signs
-  # @return [String] epured string
-  def get_int(sign = true)
-    return self.gsub(/[^\-\+\d]/, "") if sign == true
-    return self.gsub(/[^\d]/, "")
-  end
-
-  # see {#get_int}
-  #
-  # @return [String]
-  def get_int!(sign = true)
-    return self.replace(self.get_int(sign))
-  end
-
-  # get only the digits and + - . , symbols in the string
-  # you can also see {#to_fi} to turn the String into a Float
-  #
-  # @param sign [true or false] if true, keep the - and + signs
-  # @return [String] epured_string
-  def get_float(sign = true)
-    return self.gsub(/[^\-\+\d\.\,]/, "") if sign == true
-    return self.gsub(/[^\d\.\,]/, "") if sign == true
-  end
-
-  # see {#get_float}
-  #
-  # @return [String]
-  def get_float!(sign = true)
-    return self.replace(self.get_float(sign))
-  end
-
   # Capitalize a sequence (each word)
   #
   # @return [String] capitalized_string
@@ -291,15 +239,6 @@ module StringHelper
   # @return [Array]
   def splity!(sep)
     return self.replace(self.splity(sep))
-  end
-
-  # transforme the string into an float in m² if containing "ha"
-  #
-  # @return [Float]
-  def ha2m2
-    return self.gsub("m2", "").get_float.to_fi unless self.match(/(\A|[^[:alpha:]])ha(\Z|[^[:alpha:]])/i)
-    return self.gsub("m2", "").get_float.to_fi * 10_000 unless self.match(/(\A|[^[:alpha:]])m(2|²)(\Z|[^[:alpha:]])/i)
-    return self.gsub("m2", "").get_float.to_fi
   end
 
 end
