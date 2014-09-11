1bbf9ee9644bf073ef620b54c9b42adc7a68ba39
Arthur Poulet <arthur.poulet@cryptolab.net>
2014-09-11 21:02:05 +0200

    Add #get_floats and #get_ints features

--- lib/rubyhelper/string_numeric.rb
+++ lib/rubyhelper/string_numeric.rb
@@ -23,10 +23,11 @@ module StringNumericHelper
   # get only the digits and + - symbols in the string
   # you should see also {#get_float}
   #
-  # @param sign (true/false) if true, keep the - and + signs
+  # @param sign [true or false or :less ] if true, keep the - and + signs, if :less, only keep -
   # @return [String] epured string
   def get_int(sign = true)
     return self.gsub(/[^\-\+\d]/, "") if sign == true
+    return self.gsub(/[^\-\d]/, "") if sign == :less
     return self.gsub(/[^\d]/, "")
   end
 
@@ -38,12 +39,13 @@ module StringNumericHelper
   end
 
   # get only the digits and + - symbols in the string of the first group of digits
-  # you should see also {#get_int} and {#get_1float}
+  # you should see also {#get_int} {#get_ints} and {#get_1float}
   #
-  # @param sign (true/false) if true, keep the - and + signs
+  # @param sign [true or false or :less ] if true, keep the - and + signs, if :less, only keep -
   # @return [String] epured string
   def get_1int(sign = true)
     return self.match(/([\d\-\+]*\d[\d\-\+]*)/).to_a[1].to_s.get_int(sign) if sign == true
+    return self.match(/([\d\-  ]*\d[\d\-]*)/).to_a[1].to_s.get_int(sign) if sign == :less
     return self.match(/(\d+)/).to_a[1].to_s.get_int(sign)
   end
 
@@ -54,13 +56,28 @@ module StringNumericHelper
     return self.replace(self.get_1int(sign))
   end
 
+  # get all integer groups
+  # if sep is a sign and the param sign == true, then theses signs will be splited first
+  # see also {#get_floats} and {#get_int}
+  #
+  # @param sep [String or Regexp] separator
+  # @param sign [true or false or :less ] if true, keep the - and + signs, if :less, only keep -
+  # @raise ArgumentError if sep is not a String
+  # @return [Array of String]
+  def get_ints(sep = ' ', sign = true)
+    raise ArgumentError, "sep must be a String" unless sep.is_a? String or sep.is_a? Regexp
+    return self.split(sep).map{|e| e.get_int(sign)}
+  end
+
   # get only the digits and + - . , symbols in the string
   # you can also see {#to_fi} to turn the String into a Float
+  # see als {#get_1float} {#get_floats} and {#get_int}
   #
-  # @param sign [true or false] if true, keep the - and + signs
+  # @param sign [true or false or :less ] if true, keep the - and + signs, if :less, only keep -
   # @return [String] epured_string
   def get_float(sign = true)
     return self.gsub(/[^\-\+\d\.\,]/, "") if sign == true
+    return self.gsub(/[^\-\d\.\,]/, "") if sign == :less
     return self.gsub(/[^\d\.\,]/, "")
   end
 
@@ -74,11 +91,25 @@ module StringNumericHelper
   # get only the digits and + - . , symbols in the string of the first group of digits
   # you should see also {#get_float} and {#get_1int}
   #
-  # @param sign (true/false) if true, keep the - and + signs
+  # @param sign [true or false or :less ] if true, keep the - and + signs, if :less, only keep -
   # @return [String] epured string
   def get_1float(sign = true)
     return self.match(/([\d\.,\-\+]*\d[\d\.,\-\+]*)/).to_a[1].to_s.get_float(sign) if sign == true
+    return self.match(/([\d\.,\-]*\d[\d\.,\-]*)/).to_a[1].to_s.get_float(sign) if sign == :less
     return self.match(/([\d\.,]*\d[\d\.,]*)/).to_a[1].to_s.get_float(sign)
+  end
+
+  # get all floats groups
+  # if sep is a sign and the param sign == true, then theses signs will be splited first
+  # see also {#get_float} and {#get_ints}
+  #
+  # @param sep [String or Regexp] separator
+  # @param sign [true or false or :less ] if true, keep the - and + signs, if :less, only keep -
+  # @raise ArgumentError if sep is not a String
+  # @return [Array of String]
+  def get_floats(sep = ' ', sign = true)
+    raise ArgumentError, "sep must be a String" unless sep.is_a? String or sep.is_a? Regexp
+    return self.split(sep).map{|e| e.get_float(sign)}
   end
 
   # see {#get_1float}
